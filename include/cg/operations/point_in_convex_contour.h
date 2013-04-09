bb5c9402dd2aae9d5f795f0fa55523dbddd69b3d
Anton Kovsharov <antonkovsharov@gmail.com>
2013-04-09 21:17:22 +0400

    dfa

--- include/cg/operations/point_in_convex_contour.h
+++ include/cg/operations/point_in_convex_contour.h
@@ -7,21 +7,13 @@
 namespace cg {
     template <typename T>
     bool point_in_convex_contour(point_2t<T> p, contour_2t<T> cont) {
-        if (orientation(cont[0], cont[1], p) == CG_RIGHT || orientation(cont[0], cont[cont.vertices_num() - 1], p) == CG_LEFT) {
-            return false;
-        }
-        typedef typename std::vector<point_2t<T> >::const_iterator const_iterator;
-
-        const_iterator it = std::lower_bound(cont.begin(), cont.end(), p, [&cont](const point_2t<T> &lhs, const point_2t<T> &rhs) {
-                         return orientation(cont[0], lhs, rhs) != CG_RIGHT;
-        });
-
-        if (it == cont.end()) {
-            it--;
-        }
-        point_2t<T> second = (*it);
-        point_2t<T> first = (*(--it));
-
-        return orientation(first, second, p) != CG_RIGHT;
+       if (orientation(cont[0], cont[1], p) == CG_RIGHT) return false;
+       auto it = std::lower_bound(cont.begin() + 2, cont.end(), p, [&cont](const point_2t<T> &lhs, const point_2t<T> &rhs) {
+         return orientation(cont[0], lhs, rhs) == CG_LEFT;
+       });
+    if (it == cont.end()) {
+        return false;
+    }
+    return orientation(*(it - 1), *it, p) != CG_RIGHT;
      }
 }
