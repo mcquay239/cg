41c78d879ea9a1c7f044928be5bb7d8e8d019c7a
Mikhail Melnik <by.zumzoom@gmail.com>
2013-05-18 02:53:49 +0400

    [+] Dynamic hull interface

---
+++ include/cg/convex_hull/dynamic_hull_interface.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <cg/primitives/point.h>
+
+namespace cg
+{
+   template <class Iterator>
+   struct dynamic_hull_interface
+   {
+      virtual void add_point(point_2 p) = 0;
+      virtual void remove_point(const point_2& p) = 0;
+      virtual const std::pair<Iterator, Iterator> get_hull() = 0;
+      virtual const std::pair<Iterator, Iterator> get_all_points() = 0;
+      virtual ~dynamic_hull_interface() {}
+   };
+}
+
