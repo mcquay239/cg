bfc3fd691aebfa52734d1b22563c6b5719937025
Anton Kovsharov <antonkovsharov@gmail.com>
2013-07-10 01:13:40 +0600

    simple test added

--- tests/triangulation.cpp
+++ tests/triangulation.cpp
@@ -7,11 +7,18 @@
 
 using namespace std;
 
+TEST(triangulation, simple_test) {
+    cg::contour_2 outer({ cg::point_2(-1, -1), cg::point_2(1, -1), cg::point_2(0, 1), cg::point_2(-1, 1) });
+    vector<cg::triangle_2> v = cg::triangulate({ outer });
+    EXPECT_TRUE(v.size() == 2);
+}
+
+
 TEST(triangulation, init_test) {
     cg::contour_2 outer({ cg::point_2(-2, -2), cg::point_2(2, -2), cg::point_2(2, 2), cg::point_2(-2, 2) });
     cg::contour_2 hole({ cg::point_2(1, 1), cg::point_2(1, -1), cg::point_2(-1, -1), cg::point_2(-1, 1) });
     vector<cg::triangle_2> v = cg::triangulate({ outer, hole });
-    EXPECT_TRUE(v.size() == 1);
+    EXPECT_TRUE(v.size() == 8);
 }
 
 int main(int argc, char **argv) {
