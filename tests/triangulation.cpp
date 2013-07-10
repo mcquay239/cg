addb16aed848a6e68d9b9f77bf313468efcaa0ac
Anton Kovsharov <antonkovsharov@gmail.com>
2013-07-10 12:44:06 +0600

    bugs fixed

--- tests/triangulation.cpp
+++ tests/triangulation.cpp
@@ -19,11 +19,13 @@ bool inner_intersection(const segment_2 & a, const segment_2 &b) {
     for (size_t l = 0; l != 2; ++l) 
         ab[l] = orientation(a[0], a[1], b[l]);
 
-    if (ab[0] == ab[1] && ab[0] == CG_COLLINEAR) 
+    if (ab[0] == CG_COLLINEAR || ab[1] == CG_COLLINEAR) 
         return false;
     if (ab[0] == ab[1]) return false;
     for (size_t l = 0; l != 2; l++) 
         ab[l] = orientation(b[0], b[1], a[l]);
+    if (ab[0] == CG_COLLINEAR || ab[1] == CG_COLLINEAR) 
+        return false;
     return ab[0] != ab[1];
 }
 
@@ -46,9 +48,9 @@ bool check_triangulation(polygon poly, vector<triangle_2> t) {
     //normal triangles
     size_t count_v = 0;
     for (auto cont : poly) count_v += cont.vertices_num();
-    if (t.size() != count_v + 2) return false;
+    EXPECT_TRUE(t.size() == count_v + 2 * (poly.size() - 2)); 
     for (auto tr : t) {
-        if (orientation(tr[0], tr[1], tr[2]) == CG_COLLINEAR) return false;
+        EXPECT_FALSE(orientation(tr[0], tr[1], tr[2]) == CG_COLLINEAR);
     }
     //which not intersect
     //(intersections with touches allowed
@@ -57,7 +59,7 @@ bool check_triangulation(polygon poly, vector<triangle_2> t) {
             auto t1 = t[i], t2 = t[j];
             for (int k = 0; k < 3; k++) {
                 auto s = t1.side(k);
-                if (inner_intersection(s, t2)) return false;
+                EXPECT_FALSE(inner_intersection(s, t2));
             }
         }
     }
@@ -68,16 +70,18 @@ bool check_triangulation(polygon poly, vector<triangle_2> t) {
 
 TEST(triangulation, simple_test) {
     contour_2 outer({ point_2(-1, -1), point_2(1, -1), point_2(0, 1), point_2(-1, 1) });
-    vector<triangle_2> v = triangulate({ outer });
-    EXPECT_TRUE(v.size() == 2);
+    auto poly = {outer};
+    vector<triangle_2> v = triangulate(poly);
+    check_triangulation(poly, v);
 }
 
 
 TEST(triangulation, init_test) {
     contour_2 outer({ point_2(-2, -2), point_2(2, -2), point_2(2, 2), point_2(-2, 2) });
     contour_2 hole({ point_2(1, 1), point_2(1, -1), point_2(-1, -1), point_2(-1, 1) });
-    vector<triangle_2> v = triangulate({ outer, hole });
-    EXPECT_TRUE(v.size() == 8);
+    auto poly = {outer, hole};
+    vector<triangle_2> v = triangulate(poly);
+    check_triangulation(poly, v);
 }
 
 int main(int argc, char **argv) {
