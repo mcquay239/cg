52284d5a1d63d385a9cc699039f6a506def59b07
Grigory <grtkachenko@gmail.com>
2013-10-16 03:00:11 +0400

    Added values to common dir and cosmetic changes in shortest_path

--- include/cg/visibility/shortest_path.h
+++ include/cg/visibility/shortest_path.h
@@ -1,25 +1,13 @@
 #pragma once
-
-#include <cg/operations/orientation.h>
-#include <vector>
-#include <iostream>
-#include <algorithm>
-#include <iterator>
-#include <algorithm>
-#include <cg/primitives/segment.h>
-#include <cg/primitives/point.h>
-#include <cg/primitives/contour.h>
 #include <cg/visibility/visibility3.h>
+#include <cg/common/values.h>
 #include <map>
-#include <math.h>
 
 namespace cg
 {
-   const double MAX_VALUE = 1e18;
-
    template<class Scalar>
-   void add_point(int * num, point_2t<Scalar> const& p, std::map<point_2t<Scalar>, int>& number_by_point,
-                  std::map<int, point_2t<Scalar> >& point_by_number)
+   void add_point(int * num, point_2t<Scalar> const & p, std::map<point_2t<Scalar>, int> & number_by_point,
+                  std::map<int, point_2t<Scalar> > & point_by_number)
    {
       number_by_point[p] = *num;
       point_by_number[*num] = p;
@@ -28,14 +16,14 @@ namespace cg
 
    std::vector<int> get_path(std::vector<std::vector<std::pair<int, double> > > const& a, int start, int end, int size)
    {
-      std::vector<double> d(size, MAX_VALUE);
-      std::vector<int> par(size, -1);
+      std::vector<double> d(size, DOUBLE_INF);
+      std::vector<int> par(size, ILLEGAL_VERTEX);
       std::vector<bool> used(size, false);
       d[start] = 0;
       for (int i = 0; i < size; i++)
       {
-         double dmin = MAX_VALUE;
-         int index = -1;
+         double dmin = DOUBLE_INF;
+         int index = ILLEGAL_VERTEX;
          for (int j = 0; j < size; j++)
          {
             if (!used[j] && dmin >= d[j])
@@ -69,20 +57,20 @@ namespace cg
       return ans;
    }
 
+   // TODO: After accepting pull request #30 get rid of it
    template <class Scalar>
-   double distance(point_2t<Scalar> const& a, point_2t<Scalar> const& b)
+   inline double distance(point_2t<Scalar> const & a, point_2t<Scalar> const & b)
    {
-      vector_2t<Scalar> vect(a.x - b.x, a.y - b.y);
+      auto vect(a - b);
       return sqrt(vect * vect);
    }
 
    template <class Scalar>
-   std::vector<point_2t<Scalar> > get_shortest_path(point_2t<Scalar> const& start, point_2t<Scalar> const& end, std::vector<contour_2t<Scalar> >& polygons)
+   std::vector<point_2t<Scalar> > get_shortest_path(point_2t<Scalar> const & start, point_2t<Scalar> const & end, std::vector<contour_2t<Scalar> > & polygons)
    {
       typedef contour_2t<Scalar> Contour;
       typedef segment_2t<Scalar> Segment;
       typedef point_2t<Scalar> Point;
-
 
       std::vector<Segment> vis_graph = get_visibility_graph(start, end, polygons);
       std::map<Point, int> number_by_point;
@@ -110,9 +98,7 @@ namespace cg
       }
       auto ans_points = get_path(a, 0, size - 1, size);
       for (int i : ans_points)
-      {
          ans.push_back(point_by_number[i]);
-      }
 
       return ans;
    }
