baddf1dd2aeabf5a75a7b255852e89278e3b6828
Grigory <grtkachenko@gmail.com>
2013-10-17 16:39:32 +0400

    Tab refactoring tab -> 3 spaces

--- tests/visibility_naive.cpp
+++ tests/visibility_naive.cpp
@@ -16,11 +16,11 @@ using cg::segment_2;
 TEST(visibility, sample)
 {
    contour_2 start( {point_2(0, 0)} ), finish( {point_2(4, 4)} );
-	vector<contour_2> poly;
-	std::vector<point_2> pts = boost::assign::list_of(point_2(1, 1))
-	(point_2(2, 1))
-	(point_2(2, 2))
-	(point_2(1, 2));
+   vector<contour_2> poly;
+   std::vector<point_2> pts = boost::assign::list_of(point_2(1, 1))
+                              (point_2(2, 1))
+                              (point_2(2, 2))
+                              (point_2(1, 2));
 
    poly.push_back(start);
    poly.push_back(pts);
@@ -38,23 +38,23 @@ TEST(visibility, hard)
    poly.push_back(start);
    poly.push_back(finish);
 
-	std::vector<point_2> first = boost::assign::list_of(point_2(5, 1))
-	(point_2(5, 5))
-	(point_2(3, 3));
+   std::vector<point_2> first = boost::assign::list_of(point_2(5, 1))
+                                (point_2(5, 5))
+                                (point_2(3, 3));
 
-	poly.push_back(first);
+   poly.push_back(first);
 
-	std::vector<point_2> second = boost::assign::list_of(point_2(5, -1))
-	(point_2(8, -1))
-	(point_2(7, 3));
+   std::vector<point_2> second = boost::assign::list_of(point_2(5, -1))
+                                 (point_2(8, -1))
+                                 (point_2(7, 3));
 
-	poly.push_back(second);
+   poly.push_back(second);
 
-	std::vector<point_2> third = boost::assign::list_of(point_2(6, 4))
-	(point_2(8, 4))
-	(point_2(6, 5));
+   std::vector<point_2> third = boost::assign::list_of(point_2(6, 4))
+                                (point_2(8, 4))
+                                (point_2(6, 5));
 
-	poly.push_back(third);
+   poly.push_back(third);
 
    vector<segment_2> ans = cg::get_visibility_graph(poly);
    EXPECT_TRUE(ans.size() == 26);
@@ -66,13 +66,13 @@ TEST(visibility, unbelievable)
    vector<contour_2> poly;
    poly.push_back(start);
    poly.push_back(finish);
-	std::vector<point_2> first = boost::assign::list_of(point_2(1, 1))
-	(point_2(1, -2))
-	(point_2(2, 0))
-	(point_2(3, -2))
-	(point_2(3, 1));
+   std::vector<point_2> first = boost::assign::list_of(point_2(1, 1))
+                                (point_2(1, -2))
+                                (point_2(2, 0))
+                                (point_2(3, -2))
+                                (point_2(3, 1));
 
-	poly.push_back(first);
+   poly.push_back(first);
 
    vector<segment_2> ans = cg::get_visibility_graph(poly);
    EXPECT_TRUE(ans.size() == 11);
@@ -86,5 +86,5 @@ TEST(visibility, gangsta)
    poly.push_back(finish);
 
    vector<segment_2> ans = cg::get_visibility_graph(poly);
-	EXPECT_TRUE(ans.size() == 1);
+   EXPECT_TRUE(ans.size() == 1);
 }
