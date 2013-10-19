512c947638c060d3cc1d3212ee1a02c9c9d4f92e
GodGefest <godgefest@gmail.com>
2013-10-19 05:16:00 +0400

    is_point added

--- include/cg/primitives/segment.h
+++ include/cg/primitives/segment.h
@@ -21,6 +21,8 @@ namespace cg
       point_2t<Scalar> &         operator [] (size_t id)       { return pts_[id]; }
       point_2t<Scalar> const &   operator [] (size_t id) const { return pts_[id]; }
 
+      bool is_point() { return pts_[0] == pts_[1]; }
+
    private:
       boost::array<point_2t<Scalar>, 2 > pts_;
    };
